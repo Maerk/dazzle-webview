6cf6fa0a7cf201821fa7e3b289b078d4773a52f7
Marco De Nicolo <maerkdeni@gmail.com>
2017-04-03 21:46:36 +0200

    added colors and fixed drawPoints

--- draw.js
+++ draw.js
@@ -3,7 +3,23 @@ var players = new Map(); //map(id_player,struttura_associata)
 var first_time = true;
 var def_color = "#070d12";
 var noplay_color = "#c2c2a3";
-var colors_vec = ["#ff0000", "#00ff00", "#0000ff", "#ff33cc", "#ffff00", "#00ccff", "#ff6600", "#ff9999", "#996600", "#660066", "#ccff33", "#009999"];
+var colors_vec = [
+        "#FFFF00", "#1CE6FF", "#FF34FF", "#FF4A46", "#008941", "#006FA6", "#A30059",
+        "#FFDBE5", "#7A4900", "#0000A6", "#63FFAC", "#B79762", "#004D43", "#8FB0FF", "#997D87",
+        "#5A0007", "#809693", "#1B4400", "#4FC601", "#3B5DFF", "#4A3B53", "#FF2F80", "#012C58",
+        "#61615A", "#BA0900", "#6B7900", "#00C2A0", "#FFAA92", "#FF90C9", "#B903AA", "#D16100",
+        "#DDEFFF", "#000035", "#7B4F4B", "#A1C299", "#300018", "#0AA6D8", "#013349", "#00846F",
+        "#372101", "#FFB500", "#C2FFED", "#A079BF", "#CC0744", "#C0B9B2", "#C2FF99", "#001E09",
+        "#00489C", "#6F0062", "#0CBD66", "#EEC3FF", "#456D75", "#B77B68", "#7A87A1", "#788D66",
+        "#885578", "#FAD09F", "#FF8A9A", "#D157A0", "#BEC459", "#456648", "#0086ED", "#886F4C",
+        "#34362D", "#B4A8BD", "#00A6AA", "#452C2C", "#636375", "#A3C8C9", "#FF913F", "#938A81",
+        "#575329", "#00FECF", "#B05B6F", "#8CD0FF", "#3B9700", "#04F757", "#C8A1A1", "#1E6E00",
+        "#7900D7", "#A77500", "#6367A9", "#A05837", "#6B002C", "#772600", "#D790FF", "#9B9700",
+        "#549E79", "#FFF69F", "#201625", "#72418F", "#BC23FF", "#99ADC0", "#3A2465", "#922329",
+        "#5B4534", "#FDE8DC", "#404E55", "#0089A3", "#CB7E98", "#A4E804", "#324E72", "#6A3A4C",
+        "#83AB58", "#001C1E", "#D1F7CE", "#004B28", "#C8D0F6", "#A3A489", "#806C66", "#222800",
+        "#BF5650", "#E83000", "#66796D", "#DA007C", "#FF1A59", "#8ADBB4", "#1E0200", "#5B4E51",
+        "#C895C5", "#320033", "#FF6832", "#66E1D3", "#CFCDAC", "#D0AC94", "#7ED379", "#FEFFE6" ];
 var colors_index = 0;
 var rec_mat = []; //matrice rettangoli
 var col_mat = []; //matrice con vettori di colori dei giocatori
@@ -52,38 +68,24 @@ function destroyInputBox()
 function drawPoints()
 {
     var node = document.getElementById("points");
+    while (node.hasChildNodes())
+        node.removeChild(node.firstChild);
     var val = players.values();
     var it = val.next();
     var arr_points = [];
-    var map_points = new Map();
     while(!it.done)
     {
-        arr_points.push(it.value.points);
-        if(!map_points.has(it.value.points))
-        {
-            var arr_tmp = [];
-            var obj = {name:it.value.name, color:it.value.color};
-            arr_tmp.push(obj);
-            map_points.set(it.value.points,arr_tmp);
-        }
-        else
-        {
-            var obj = {name:it.value.name, color:it.value.color};
-            map_points.set(it.value.points, map_points.get(it.value.points).push(obj));
-        }
+        var obj = {points:it.value.points, name:it.value.name, color:it.value.color};
+        arr_points.push(obj);
         it = val.next();
     }
-    arr_points.sort();
-    for(var i=arr_points.length-1; i>=0; i--)
+    arr_points.sort(function(a, b) {return b.points - a.points;});
+    for(var i=0; i<arr_points.length; i++)
     {
-        var arr_obj = map_points.get(arr_points[i]);
-        for(var j=0; j<arr_obj.length; j++)
-        {
-            var subnode = document.createElement("div");
-            subnode.appendChild(document.createTextNode(arr_obj[j].name + ": " + arr_points[i]));
-            subnode.style = "color: " + arr_obj[j].color + ";";
-            node.appendChild(subnode);
-        }
+        var subnode = document.createElement("div");
+        subnode.appendChild(document.createTextNode(arr_points[i].name + ": " + arr_points[i].points));
+        subnode.style = "color: " + arr_points[i].color + ";";
+        node.appendChild(subnode);
     }
 }
 function drawTime(turns_left, ms_turn)
@@ -265,11 +267,6 @@ function assignColor()
         colors_index++;
     }
     else
-    {
-        for(var i=0; i<colors_vec.length; i++)
-            col = col.concat("#",(parseInt(colors_vec[i].substring(1,colors_vec[i].length-1), 16) - 0x000040).toString(16));
-        //col = Math.floor((Math.abs(Math.sin(seed) * 16777215)) % 16777215).toString(16);
         colors_index = 0;
-    }
     return col;
 }
